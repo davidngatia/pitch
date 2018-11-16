280464e19d2144bf463b50327bb854c3606dd452
davidngatia <machngatia@gmail.com>
2018-11-16 15:32:37 +0300

    modify

--- app/email.py
+++ app/email.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 subject_pref = 'Pitch'
-sender_email = 'machngat.com'
+sender_email = 'machngatia@gmail.com'
 
 def mail_message(subject,template,to,**kwargs):
     # sender_email = 
