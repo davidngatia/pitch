2e8b398ed9c64bf699dec0e745eb48940a5cb5e3
davidngatia <machngatia@gmail.com>
2018-11-19 08:55:32 +0300

    add delitions

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
-subject_pref = 'Pitch'
+subject_pref = 'pitch'
 sender_email = 'machngatia@gmail.com'
 
 def mail_message(subject,template,to,**kwargs):
